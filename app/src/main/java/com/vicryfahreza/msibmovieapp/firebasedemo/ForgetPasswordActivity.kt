ac19737617ba0b22fccce623d37ae13d2552201b
TIAS23 <96928599+TIAS23@users.noreply.github.com>
2023-10-20 14:59:49 +0700

    update forget password for notification mailbox

--- app/src/main/java/com/vicryfahreza/msibmovieapp/firebasedemo/ForgetPasswordActivity.kt
+++ app/src/main/java/com/vicryfahreza/msibmovieapp/firebasedemo/ForgetPasswordActivity.kt
@@ -1,17 +1,51 @@
 package com.vicryfahreza.msibmovieapp.firebasedemo
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.vicryfahreza.msibmovieapp.R
+import android.text.TextUtils
+import android.util.Patterns
+import android.view.View
+import com.google.firebase.auth.FirebaseAuth
+import com.vicryfahreza.msibmovieapp.databinding.ActivityForgetPasswordBinding
 
-class ForgetPasswordActivity : AppCompatActivity() {
+
+class ForgetPasswordActivity : BaseActivity() {
+    private var binding: ActivityForgetPasswordBinding? = null
+    private lateinit var auth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_forget_password)
+        binding = ActivityForgetPasswordBinding.inflate(layoutInflater)
+        setContentView(binding?.root)
+        auth = FirebaseAuth.getInstance()
+        binding?.btnForgotPasswordSubmit?.setOnClickListener {
+            resetPassword()
+        }
     }
 
-    override fun onBackPressed() {
-        super.onBackPressed()
-        finish()
+    private fun resetPassword() {
+        val email = binding?.etForgotPasswordEmail?.text.toString()
+        if (validateEmail(email)) {
+            showProgressBar()
+            auth.sendPasswordResetEmail(email).addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    binding?.tilEmailForgetPassword?.visibility = View.GONE
+                    binding?.tvSubmitMsg?.visibility = View.VISIBLE
+                    binding?.btnForgotPasswordSubmit?.visibility = View.GONE
+                    hideProgressBar()
+                } else {
+                    hideProgressBar()
+                    showToast(this, "Reset password failed, try again later")
+                }
+            }
+        }
+    }
+
+    private fun validateEmail(email: String): Boolean {
+        return if (TextUtils.isEmpty(email) && !Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            binding?.tilEmailForgetPassword?.error = "Enter valid email address"
+            false
+        } else {
+            binding?.tilEmailForgetPassword?.error = null
+            true
+        }
     }
 }
