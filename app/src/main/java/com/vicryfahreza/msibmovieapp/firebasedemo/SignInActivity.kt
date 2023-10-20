8753472afa8a8ecef0543e04d58cbbef30973510
TIAS23 <96928599+TIAS23@users.noreply.github.com>
2023-10-20 08:43:59 +0700

    Menghubungkan activity sign up, sign in dan forget password

--- app/src/main/java/com/vicryfahreza/msibmovieapp/firebasedemo/SignInActivity.kt
+++ app/src/main/java/com/vicryfahreza/msibmovieapp/firebasedemo/SignInActivity.kt
@@ -1,12 +1,24 @@
 package com.vicryfahreza.msibmovieapp.firebasedemo
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.vicryfahreza.msibmovieapp.R
+import com.vicryfahreza.msibmovieapp.databinding.ActivitySignInBinding
 
 class SignInActivity : AppCompatActivity() {
+    private var binding:ActivitySignInBinding?=null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_sign_in)
+        binding = ActivitySignInBinding.inflate(layoutInflater)
+        setContentView(binding?.root)
+
+        binding?.tvRegister?.setOnClickListener{
+            startActivity(Intent(this,SignUpActivity::class.java))
+            finish()
+        }
+        binding?.tvForgotPassword?.setOnClickListener {
+            startActivity(Intent(this,ForgetPasswordActivity::class.java))
+            finish()
+        }
     }
 }
--- app/src/main/java/com/vicryfahreza/msibmovieapp/firebasedemo/SignUpActivity.kt
+++ app/src/main/java/com/vicryfahreza/msibmovieapp/firebasedemo/SignUpActivity.kt
@@ -1,12 +1,19 @@
 package com.vicryfahreza.msibmovieapp.firebasedemo
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.vicryfahreza.msibmovieapp.R
+import com.vicryfahreza.msibmovieapp.databinding.ActivitySignUpBinding
 
 class SignUpActivity : AppCompatActivity() {
+    private var binding:ActivitySignUpBinding?=null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_sign_up)
+        binding = ActivitySignUpBinding.inflate(layoutInflater)
+        setContentView(binding?.root)
+
+        binding?.tvLoginPage?.setOnClickListener {
+            startActivity(Intent(this,SignInActivity::class.java))
+        }
     }
 }
