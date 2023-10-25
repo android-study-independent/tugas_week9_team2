504ae02c3971133ba668e70bc1a03b3f7de1bed5
bangalexx <ras183375@gmail.com>
2023-10-25 10:09:17 +0700

    Menambahkan  fitur google analytics

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -44,7 +44,7 @@ android {
 
 dependencies {
 
-    implementation("androidx.core:core-ktx:1.9.0")
+    implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.10.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
@@ -56,9 +56,9 @@ dependencies {
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
     implementation("com.google.code.gson:gson:2.10.1")
     implementation("com.squareup.okhttp3:logging-interceptor:4.11.0")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.4.0")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
 
-    implementation("com.squareup.picasso:picasso:2.8")
+    implementation("com.squareup.picasso:picasso:2.71828")
     implementation("com.google.firebase:firebase-auth")
     implementation("com.google.firebase:firebase-auth-ktx:22.2.0")
     implementation("com.google.android.gms:play-services-auth:20.7.0")
--- app/src/main/java/com/vicryfahreza/msibmovieapp/firebasedemo/SignInActivity.kt
+++ app/src/main/java/com/vicryfahreza/msibmovieapp/firebasedemo/SignInActivity.kt
@@ -12,6 +12,7 @@ import com.google.android.gms.auth.api.signin.GoogleSignInClient
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import com.google.android.gms.common.api.ApiException
 import com.google.firebase.Firebase
+import com.google.firebase.analytics.FirebaseAnalytics  // Import Firebase Analytics
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.auth.GoogleAuthProvider
@@ -21,9 +22,11 @@ import com.vicryfahreza.msibmovieapp.databinding.ActivitySignInBinding
 import com.vicryfahreza.msibmovieapp.ui.MainActivity
 
 class SignInActivity : BaseActivity() {
-    private var binding:ActivitySignInBinding?=null
-    private lateinit var auth:FirebaseAuth
+    private var binding: ActivitySignInBinding? = null
+    private lateinit var auth: FirebaseAuth
     private lateinit var googleSignInClient: GoogleSignInClient
+    private lateinit var firebaseAnalytics: FirebaseAnalytics  // Inisialisasi Firebase Analytics
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivitySignInBinding.inflate(layoutInflater)
@@ -38,8 +41,10 @@ class SignInActivity : BaseActivity() {
 
         auth = Firebase.auth
 
+        firebaseAnalytics = FirebaseAnalytics.getInstance(this)  // Inisialisasi Firebase Analytics
+
         val currentUser = auth.currentUser
-        if(currentUser != null) {
+        if (currentUser != null) {
             startActivity(Intent(this@SignInActivity, MainActivity::class.java))
             finish()
         }
@@ -48,13 +53,12 @@ class SignInActivity : BaseActivity() {
             signInGoogle()
         }
 
-
-        binding?.tvRegister?.setOnClickListener{
-            startActivity(Intent(this,SignUpActivity::class.java))
+        binding?.tvRegister?.setOnClickListener {
+            startActivity(Intent(this, SignUpActivity::class.java))
             finish()
         }
         binding?.tvForgotPassword?.setOnClickListener {
-            startActivity(Intent(this,ForgetPasswordActivity::class.java))
+            startActivity(Intent(this, ForgetPasswordActivity::class.java))
             finish()
         }
         binding?.btnSignIn?.setOnClickListener {
@@ -67,9 +71,8 @@ class SignInActivity : BaseActivity() {
         resultLauncher.launch(signInIntent)
     }
 
-    private var resultLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()
-    ) { result ->
-        if(result.resultCode == Activity.RESULT_OK) {
+    private var resultLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+        if (result.resultCode == Activity.RESULT_OK) {
             val task = GoogleSignIn.getSignedInAccountFromIntent(result.data)
             try {
                 val account = task.getResult(ApiException::class.java)!!
@@ -85,61 +88,67 @@ class SignInActivity : BaseActivity() {
         val credential = GoogleAuthProvider.getCredential(idToken, null)
         auth.signInWithCredential(credential)
             .addOnCompleteListener(this) { task ->
-                if(task.isSuccessful) {
+                if (task.isSuccessful) {
                     Log.d(TAG, "signInWithCredential:success")
                     val user = auth.currentUser
                     updateUI(user)
+                    // Pelacakan parameter "login_method" saat pengguna berhasil masuk dengan Google
+                    logLoginEvent("login_with_google")
                 } else {
                     Log.w(TAG, "signInWithCredential:failure", task.exception)
                     updateUI(null)
                 }
             }
-
     }
 
     private fun updateUI(currentUser: FirebaseUser?) {
-        if(currentUser != null) {
+        if (currentUser != null) {
             startActivity(Intent(this@SignInActivity, MainActivity::class.java))
             finish()
         }
     }
 
-
-    private fun signInUser()
-    {
+    private fun signInUser() {
         val email = binding?.etSinInEmail?.text.toString()
         val password = binding?.etSinInPassword?.text.toString()
-        if(validateForm(email,password))
-        {
+        if (validateForm(email, password)) {
             showProgressBar()
-            auth.signInWithEmailAndPassword(email,password)
+            auth.signInWithEmailAndPassword(email, password)
                 .addOnCompleteListener { task ->
-                    if (task.isSuccessful)
-                    {
+                    if (task.isSuccessful) {
                         startActivity(Intent(this, MainActivity::class.java))
                         finish()
                         hideProgressBar()
-                    }
-                    else
-                    {
-                        showToast(this,"Cannot login successfully. Try after sometinc")
+                        // Pelacakan parameter "login_method" saat pengguna berhasil masuk dengan metode login biasa
+                        logLoginEvent("login_with_userpass")
+                    } else {
+                        showToast(this, "Cannot login successfully. Try after some time")
                         hideProgressBar()
                     }
                 }
         }
     }
+
     private fun validateForm(email: String, password: String): Boolean {
         return when {
-            TextUtils.isEmpty(email) && !Patterns.EMAIL_ADDRESS.matcher(email).matches()-> {
-                binding?.tilEmail?.error = "Enter valid email addres"
+            TextUtils.isEmpty(email) && !Patterns.EMAIL_ADDRESS.matcher(email).matches() -> {
+                binding?.tilEmail?.error = "Enter a valid email address"
                 false
             }
-            TextUtils.isEmpty(password)->{
-                binding?.tilPassword?.error = "Enter password"
+            TextUtils.isEmpty(password) -> {
+                binding?.tilPassword?.error = "Enter a password"
                 false
             }
-            else -> {true}
+            else -> {
+                true
+            }
         }
+    }
+
+    private fun logLoginEvent(loginMethod: String) {
+        val bundle = Bundle()
+        bundle.putString("login_method", loginMethod)
+        firebaseAnalytics.logEvent("user_login", bundle)
     }
 
     companion object {
